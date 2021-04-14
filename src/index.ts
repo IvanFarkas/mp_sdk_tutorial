b93182deb980288299fafa2fb84a62fe0f46cd79
rolando <rgarro@gmail.com>
2021-04-13 19:35:04 -0600

    still figuring out how to start fbx model animations ..

--- src/index.ts
+++ src/index.ts
@@ -658,7 +658,7 @@ class App {
     //const url = 'https://gitcdn.link/repo/mrdoob/three.js/dev/examples/models/fbx/stanford-bunny.fbx';
     //const url = 'http://localhost:8000/parrot_bebop_droneB.fbx';
     const url = 'http://localhost:8000/parrot_bebop_droneAnimated.fbx';
-
+    //const url = 'http://localhost:8000/parrot_bebop_droneAnimated6.fbx';
     const initial = {
       url: url,
       visible: true,
