eff6e16031e821e8111318cdcf3f9f515382bdde
IvanFarkas <Ivan.Farkas@3DHDSoft.com>
2021-06-02 12:21:39 -0400

    Formatted

--- src/core/NavigationSystem.ts
+++ src/core/NavigationSystem.ts
@@ -3,6 +3,7 @@ import { Pathfinding, PathfindingHelper } from './pathfinding';
 
 const ZONE = 'level';
 const SPEED = 50;
+
 export default class NavigationSystem {
   groupID: any;
   path: any;
@@ -59,7 +60,9 @@ export default class NavigationSystem {
     const closestTargetNode: any = this.pathfinder.getClosestNode(this.targetPosition, ZONE, targetGroupID, true);
 
     this.helper.setTargetPosition(this.targetPosition);
-    if (closestTargetNode) this.helper.setNodePosition(closestTargetNode.centroid);
+    if (closestTargetNode) {
+      this.helper.setNodePosition(closestTargetNode.centroid);
+    }
 
     // Calculate a path to the target and store it
     this.path = this.pathfinder.findPath(this.playerPosition, this.targetPosition, ZONE, this.groupID);
@@ -80,21 +83,24 @@ export default class NavigationSystem {
   public animate(): void {
     requestAnimationFrame(this.animate.bind(this));
 
-    if (!(this.path || []).length) return;
+    if (!(this.path || []).length) {
+      return;
+    }
 
     let targetPosition = this.path[0];
     const velocity = targetPosition.clone().sub(this.playerPosition);
     if (velocity.lengthSq() > 0.05 * 0.05) {
       velocity.normalize();
-      //Move player to target
+
+      // Move player to target
       this.playerPosition.add(velocity.multiplyScalar(this.clock.getDelta() * SPEED));
       this.helper.setPlayerPosition(this.playerPosition);
 
-      //Follow helper
+      // Follow helper
       this.playerNode.position.set(this.playerPosition.x, this.playerPosition.y, this.playerPosition.z);
       // this.playerNode.quaternion.set(this.helper.quaternion.x, this.helper.quaternion.y, this.helper.quaternion.z, this.helper.quaternion.w);
     } else {
-      //Remove node from the path calculated
+      // Remove node from the path calculated
       this.path.shift();
     }
   }
